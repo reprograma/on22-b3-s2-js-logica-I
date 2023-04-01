5a7b4a637c424e71f07e813e6e24446a97995b33
Iasmin <lucas_tadsiq@hotmail.com>
2023-04-01 19:36:35 -0300

    Exercício começado

---
+++ exercicios/para-casa/exercicio.js
@@ -0,0 +1,24 @@
+//Exercício 1:
+
+//Crie uma variável chamada "nome" e atribua a ela seu nome completo como uma string. 
+//Em seguida, crie uma segunda variável chamada "idade" e atribua a ela sua idade como um número. 
+//Finalmente, exiba no console uma mensagem que diga "Meu nome é [nome] e tenho [idade] anos".
+
+let nome = "Iasmin de Almeida Dias Siqueira"
+let idade = 18
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos.")
+
+//Exercício 2:
+
+//Crie uma variável chamada "saldo" e atribua a ela um valor numérico que represente o saldo da sua conta 
+//bancária. Em seguida, crie uma segunda variável chamada "gastos" e atribua a ela um valor numérico que 
+//represente a quantia de dinheiro que você gastou em um mês. 
+//Finalmente, crie uma terceira variável chamada "saldoFinal" 
+//que represente o saldo da sua conta após subtrair seus gastos do saldo inicial. 
+//Exiba no console uma mensagem que diga "Meu saldo final é de [saldoFinal] reais".
+
+let saldo = 1000
+let gastos = 500
+let saldoFinal = saldo - gastos
+console.log("Meu saldo final é de" + saldoFinal + "reais.")
---
+++ exercicios/para-sala/variaveis.js
@@ -0,0 +1,5 @@
+let qtdaAlunas = 44
+let professora = "Manuelly Suzik"
+
+console.log(qtdaAlunas)
+console.log(professora)
