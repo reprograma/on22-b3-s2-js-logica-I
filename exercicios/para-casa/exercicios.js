08c753c4efcc79ef7f840efcb47d57d189d62864
CybelleCouto <cybelinhacouto@hotmail.com>
2023-04-02 14:18:48 -0300

    exercicio s2 resolvido

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,16 @@
 // Exercicio 1
-
-
+let nome = "Cybelle Couto"
+let idade = 27
+console.log("Meu nome é " + nome + " e tenho " + 
+idade +" anos."
+)
 
 // Exercicio 2
+
+let saldo = 50.00
+let gastos = 1000.00
+let saldoFinal = (saldo) - (gastos)
+console.log("Meu saldo é " + saldo + " reais" +
+" meus gastos são " + gastos + " reais" +
+ " e meu saldo final é "+ saldoFinal + " reais."
+ + " que situação '-- ")
