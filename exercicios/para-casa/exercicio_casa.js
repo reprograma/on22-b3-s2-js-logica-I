d9e7b3ddb53ece39887393ed7504bfeee55912c7
Francielly Kelly <fransilva.fks@gmail.com>
2023-04-02 19:56:54 -0300

    Exercício para casa da semana 2

---
+++ exercicios/para-casa/exercicio_casa.js
@@ -0,0 +1,18 @@
+//Exercício 1
+
+let nome = "Francielly Kelly"
+let idade = 23
+
+//console.log("Meu nome é", nome, "tenho", idade, "anos") // concatenação
+console.log(`Meu nome é ${nome}, tenho ${idade} anos`) // template literals
+
+
+//Exercicio 2 
+
+let saldo = 1000
+let gastos = 800
+
+let saldoFinal = saldo - gastos
+
+console.log(`Meu saldo final é ${saldoFinal}`)
+
