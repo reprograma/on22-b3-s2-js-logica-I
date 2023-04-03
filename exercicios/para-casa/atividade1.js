3dc8c6ca45e26e35f21f09fbd86263a75d233ef6
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-04-03 16:30:12 -0300

    Concluindo a atividade de casa

---
+++ exercicios/para-casa/atividade1.js
@@ -0,0 +1,3 @@
+const nome = 'Ângela Lúcia do Nascimento Oliveira'
+const idade = 25
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
---
+++ exercicios/para-casa/atividade2.js
@@ -0,0 +1,4 @@
+const saldo = 300
+const gasto = 250
+const saldoFinal = (saldo - gasto)
+console.log(`Meu saldo final é de R$${saldoFinal}`)
