ce3f8799b44e4ff7727e79580a242ac47d8a2567
nearlyerika <erika.melloramos@gmail.com>
2023-04-02 22:31:26 -0300

    home exerc 1 done

---
+++ material/exercicio_casa_1.js
@@ -0,0 +1,10 @@
+//
+//
+
+//variable declaration & initialization
+let nome = "Erika de Mello Ramos"
+let idade = 27
+
+//exercize resolution
+//method: string & variable concat
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos.")
