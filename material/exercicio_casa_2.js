cbac8e38326a3994fae125d9aa2ac61965461f8f
nearlyerika <erika.melloramos@gmail.com>
2023-04-02 22:43:25 -0300

    home exerc 2 done

---
+++ material/exercicio_casa_2.js
@@ -0,0 +1,15 @@
+//
+//
+
+//varibles declar & init
+let saldo = 1042
+let gastos = 955
+
+//exercise resolution
+let saldoFinal = saldo - gastos
+
+//toFixed method to add trailing zeroes
+saldoFinal = saldoFinal.toFixed(2)
+
+//log
+console.log("Meu saldo final é de R$" + saldoFinal + " reais.")   
