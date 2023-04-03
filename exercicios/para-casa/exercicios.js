01f8d50e03252303972593e01d2350cd914ccfc8
Juliana da Silva <julianna.furtado@gmail.com>
2023-04-03 18:47:20 -0300

    Adicionando exercício de casa.

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,14 @@
 // Exercicio 1
+let nome = "Juliana da Silva Furtado"
+let idade = 27
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos.")
 
 
 
 // Exercicio 2
+let saldo = 300
+let gastos = 200
+saldoFinal = saldo - gastos
+
+console.log("Meu saldo final é de " + saldoFinal + " reais.")
