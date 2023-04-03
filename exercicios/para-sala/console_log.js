6ccd7a617259945164f6d15babeb1f2da1936006
paiol <robertamilenapereira035@gmail.com>
2023-04-03 18:13:45 -0300

    exercíco  para-casa resolvido

--- exercicios/para-sala/console_log.js
+++ exercicios/para-sala/console_log.js
@@ -1,2 +1,5 @@
 //console.log() permite que o node execute uma mensagem que você escreveu para ser exibida no terminal
 console.log("Essa mensagem aparecerá no console do navegador, ou no console do computador onde seu arquivo JS estará sendo executado")
+
+console.log ("Olá Mundo!")
+console.log ("demorei mas achei a forma de excluir um fork do git e começar tudo de novo")
