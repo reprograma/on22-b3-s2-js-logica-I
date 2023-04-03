9896f9e388991d1b26a15ba442fc4beacb0c133d
Aline <aline.monteiro100@gmail.com>
2023-04-03 20:50:54 -0300

    Exercícios para casa: treinando variáveis.

---
+++ exercicios/para-casa/exercicio.js
@@ -0,0 +1,14 @@
+// Exercício 01:
+
+let nome = "Aline Lima Monteiro";
+let idade = 25;
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+
+// Exeercício 02:
+
+let saldo = 100000;
+let gastos = 15000;
+let saldoFinal = saldo - gastos;
+
+console.log(`Meu saldo final é: ${saldoFinal} reais.`);
