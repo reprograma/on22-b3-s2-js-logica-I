4e9b0c7e2ff33c6494d6fbde1740049090da2178
Jamile <milli.santana2017@gmail.com>
2023-04-03 19:55:12 -0300

    feat: Resolvendo exercicios para casa

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,74 @@
 // Exercicio 1
 
+let meuNome = "Jamile Santana da Silva";
+
+let minhaIdade = 34;
+
+console.log("Meu nome é", meuNome, "e tenho", minhaIdade, "anos.");
 
 
 // Exercicio 2
+
+// saldo negativo
+let saldo = -35;
+
+let gastos = 786;
+
+let saldoFinal = saldo - gastos;
+
+console.log("Meu saldo final é de", saldoFinal, "reais");
+
+// Quem dera
+// let saldo = 1000000;
+
+// let gastos = 50000;
+
+// let saldoFinal = saldo - gastos;
+
+// console.log("Meu saldo final é de", saldoFinal, "reais");
+
+// let saldo = 340;
+
+// let gastos = 340;
+
+// let saldoFinal = saldo - gastos;
+
+// console.log("Meu saldo final é de", saldoFinal, "reais");
+
+
+// Estes cálculos me retornam NaN, pois tenho varíaveis undefined
+// let saldo;
+
+// let gastos = 786;
+
+// let saldoFinal = saldo - gastos;
+
+// console.log("Meu saldo final é de", saldoFinal, "reais");
+
+// let saldo;
+
+// let gastos;
+
+// let saldoFinal = saldo - gastos;
+
+// console.log("Meu saldo final é de", saldoFinal, "reais");
+
+
+// Esse cálculo me retorna negativo, poi estou subtraindo meus gastos de 0;
+// let saldo = null;
+
+// let gastos = 786;
+
+// let saldoFinal = saldo - gastos;
+
+// console.log("Meu saldo final é de", saldoFinal, "reais");
+
+
+// Este me retorna 0 pois tenho duas variáveis null(0)
+// let saldo = null;
+
+// let gastos = null;
+
+// let saldoFinal = saldo - gastos;
+
+// console.log("Meu saldo final é de", saldoFinal, "reais");
