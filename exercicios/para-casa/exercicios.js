191c55b4787ac8b734cfc5623a31ad308b672261
unknown <luanamellof@hotmail.com>
2023-04-03 17:50:30 -0300

    mod- entrega do exercicio de casa

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,14 @@
 // Exercicio 1
 
+let meuNome = "Luana Melo Figueirêdo"
+let idade = "26"
 
+console.log("Meu nome é",meuNome, "e tenho",idade, "anos." )
 
 // Exercicio 2
+
+let saldoBancario = 5000
+let valorGostoNoMes = 3257
+let saldoFinal = (saldoBancario - valorGostoNoMes)
+
+console.log("Meu saldo final é de", saldoFinal, "reais.")
