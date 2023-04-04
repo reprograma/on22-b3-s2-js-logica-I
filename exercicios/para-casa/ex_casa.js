13e6783db49172a532dba734a04590a4fa0cbf66
roselira <roseliramary1@gmail.com>
2023-04-04 06:52:04 -0300

    exer-variaveis-js

---
+++ exercicios/para-casa/ex_casa.js
@@ -0,0 +1,9 @@
+//exercicio1
+let nome = "Rosemery Carlos Lira";
+let idade = 100;
+console.log  ("Meu nome é "+ nome+" tenho "+ idade+" anos")
+gi//exercicio1
+let saldo = 500;
+let gastos = 1000;
+let saldoFinal = saldo - gastos;
+console.log  ("Meu saldo final é de "+ saldoFinal+ " reais")
