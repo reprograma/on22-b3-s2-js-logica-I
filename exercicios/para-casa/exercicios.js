69be344cb1104b6a72da4138036e141a6d56e954
Kuni <juliangdolago@gmail.com>
2023-04-03 21:33:14 -0300

    Exercicio feito

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,18 @@
 // Exercicio 1
 
+let nome = "Julian Gaspar do Lago"
+let idade = 18
+
+console.log(`Meu nome é: ${nome} e eu tenho ${idade} anos`)
 
 
 // Exercicio 2
+
+let saldo = 10
+
+let gastos = 5
+
+let saldoFinal = saldo - gastos
+
+console.log(`Meu saldo final é de ${saldoFinal} reais`)
+
