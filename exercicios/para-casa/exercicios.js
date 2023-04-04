d4f525b92ec4cc31b743d8a5fbde0aaf84858728
Cilene <cilene.kinomoto@gmail.com>
2023-04-04 18:15:45 -0300

    ExercícioParaCasa_README

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,26 @@
-// Exercicio 1
+// Exercicio 1 variáveis nome e idade.
+
+let meuNome = "Jucilene Ferreira Bratti"
+
+let idade = 37 
+
+console.log(" Olá meu nome é " + [meuNome] + " e tenho " + [idade] + " anos. ")
 
 
+// Exercicio 2 variáveis financeiras doméstica
 
-// Exercicio 2
+let extratoBancario = {
+    meuSaldoInicial: 4500,
+    gastosDaFamilia: 3450,
+    meuSaldoFinal: 1050
+}
+
+console.log(' Meu saldo final é de R$ ' + [extratoBancario.meuSaldoFinal] + ' reais.')
+
+// exercício de operadores lógicos (tentativa)
+
+let a = 1 
+let b = 1
+let c = 3
+
+console.log('  a é menor e igual á b? ' + [a <= b] + ' e c é maior que a soma de a e b? ' + [c > (a+b)])
