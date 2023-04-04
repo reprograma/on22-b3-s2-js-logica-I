1d5cc746c808e8118e0f6bf53797857e2529b953
nearlyerika <erika.melloramos@gmail.com>
2023-04-04 16:00:23 -0300

    added more methos for home_exer_1

--- material/exercicio_casa_1.js
+++ material/exercicio_casa_1.js
@@ -6,5 +6,14 @@ let nome = "Erika de Mello Ramos"
 let idade = 27
 
 //exercize resolution
+console.log("vários métodos foram utilizados na resolução deste exercício, por isso a repetição de sua resolução.")
+
 //method: string & variable concat
 console.log("Meu nome é " + nome + " e tenho " + idade + " anos.")
+
+//method 2: comma separation
+console.log("Meu nome é", nome, "e tenho", idade, "anos.")
+
+//method 3: object
+let person = {nome, idade}
+console.log("Meu nome é", person.nome, "e tenho", person.idade,"anos.")
