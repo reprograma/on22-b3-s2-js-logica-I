17fe7c352851ea7b88d6ad59a5d2eb1f01b1da07
tejuazul <marianajaganshi@gmail.com>
2023-04-03 22:20:32 -0300

    Atividade de casa

---
+++ exercicios/para-casa/atividade_para_casa.js
@@ -0,0 +1,12 @@
+//EXERCICIO 1
+let nome = "Mariana Dias Alves dos Santos"
+let idade = 25
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos.")
+
+//EXERCICIO 2
+let saldo = 1500
+let gastos = 1300
+let saldoFinal = saldo - gastos
+
+console.log("Meu saldo final é de", saldoFinal, "reais.")
