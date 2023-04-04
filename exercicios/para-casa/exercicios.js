73656393e0442f543a9969d944ebf2300c20a16f
Marie <dudasrr24@gmail.com>
2023-04-04 17:10:24 -0300

    AtividadeSemanaDois

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,18 @@
-// Exercicio 1
+//Exercicio1
+
+let meuNome = "Maria Eduarda Rodrigues"
+let minhaIdade = 27
+
+console.log(`Meu nome é ${meuNome} e tenho ${minhaIdade} anos`);
 
 
 
-// Exercicio 2
+//Exercicio2
+
+let MeuSaldoBancario = 1600
+
+let TotalDeGastos = 820
+
+let MeuSaldoFinal = 780
+
+console.log(`Meu saldo final é de ${MeuSaldoBancario - TotalDeGastos} reais`);
