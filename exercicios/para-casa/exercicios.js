3ea27511f27c02a3f71095f9046cfb82a8fc7c80
Dayane <dayanec.dacunha@gmmail.com>
2023-04-04 14:51:46 -0300

    atividadeCasa

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,16 @@
-// Exercicio 1
+// Exercicio 1 
+
+let nome = "Dayane Conceição da Cunha"
+let idade = 27
+
+console.log("meu nome é " + nome + " tenho "+ idade + " anos");
 
 
 
 // Exercicio 2
+
+saldo = 600
+gastos = 450
+saldoFinal =150
+
+console.log("Meu saldo final é de " + saldoFinal + " reais")
