080c2163e67ca4685777b4d128b96f03a3f4ea22
Nazaré Almeida <126839545+nazare4lmeida@users.noreply.github.com>
2023-04-04 12:40:28 -0300

    Exercicios de casa resolvidos

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,12 @@
 // Exercicio 1
+let nomeDaAluna = "Maria de Nazare Oliveira de Almeida"
+let idadeDaAluna = 31
 
-
+console.log("Meu nome é " + nomeDaAluna + " e tenho " + idadeDaAluna + " anos ")
 
 // Exercicio 2
+let saldo = 600
+let gastos = 1500
+let saldoFinal = saldo - gastos
+
+console.log("Meu saldo final é de " + saldoFinal)
