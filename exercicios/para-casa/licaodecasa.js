93f1db02515e8a71872c16092e116287911b7f53
Nadia <ncastilhopersonal@gmail.com>
2023-04-04 16:55:37 +0100

    licao- de- casa- logica

---
+++ exercicios/para-casa/licaodecasa.js
@@ -0,0 +1,15 @@
+let pessoa = { 
+    nome: "Nadia de Oliveira Castilho",
+    idade: 36,
+}
+
+console.log("Meu nome é, ",pessoa.nome," e tenho ",pessoa.idade," anos")
+
+let extrato = {
+    saldo: 2.750000,
+    gastos: 10000,
+    saldofinal: 2.740000
+}
+
+console.log("Meu saldo final é de ",extrato.saldofinal," reais")
+
