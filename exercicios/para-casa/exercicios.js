a5cfb6f35d0f6e779491e1e275e8e75c31ee3345
Rosemizr <otapatilera@gmail.com>
2023-04-05 11:16:07 -0300

    Exercício da segunda semana

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,13 @@
 // Exercicio 1
+let nome = "Elisa Lacerda"
+let idade = 19
+let frase = `Meu nome é ${nome} e eu tenho ${idade} anos.`
 
-
-
+console.log(frase)
 // Exercicio 2
+let saldo = 3500
+let gastos = 1800
+let saldoFinal = saldo - gastos
+let resultado = `Meu saldo final é de R$${saldoFinal},00`
+
+console.log(resultado)
