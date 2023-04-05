63b635aabffbae2c19f17ddfa1a952f5975c2656
Tissianass <tissianass@gmail.com>
2023-04-04 21:44:50 -0300

    feito exercícios 1 e 2

---
+++ exercicios/para-casa/console_log.js
@@ -0,0 +1,3 @@
+console.log("olá mundo")
+console.log("isso é lógica")
+console.log("Essa mensagem aparecerá no console do navegador, ou no console do computador onde seu arquivo JS estará sendo executado")
---
+++ exercicios/para-casa/constante.js
@@ -0,0 +1,9 @@
+// declarar constante
+const MeuNome = "Tissiana"
+
+console.log(MeuNome)
+
+// concatenação de strings
+const MeuNomeCompleto = MeuNome + " Sousa Silva"
+
+console.log(MeuNomeCompleto)
