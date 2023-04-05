b377dc22037000d3ba62400e1752a8f8cfe9334b
aglayr <aglayr@alunos.utfpr.edu.br>
2023-04-05 18:18:00 -0300

    exercicio semana 2 feito

---
+++ exercicios/para-casa/exercicio.js
@@ -0,0 +1,11 @@
+let meuNome = "Aglayr Beatriz Marques Vieira"
+
+let  idade = "35"
+
+console.log (" Meu nome é ",meuNome," e tenho ",idade," anos")
+
+let saldo = "1960"
+
+let  gastoMensal = "1870"
+
+console.log ( " Meu saldo é R$ ",saldo - gastoMensal,"")
