dedda92ee9c5fb0889fb3d308e62c9cd82ec5a96
thaislmtrindadde <thais.lmtrindade@gmail.com>
2023-04-05 07:33:53 -0400

    exerc sem2 reprograma

---
+++ exercicios/para-casa/atividade.js
@@ -0,0 +1,11 @@
+let nome = "Thaís Lima Trindade"
+let idade = "37"
+console.log ("Meu nome é "+nome+" e tenho "+idade+" anos.")
+
+let saldo = 1000;
+let gastos = 1300;
+let saldofinal = saldo - gastos;
+console.log ("Meu saldo final é "+saldofinal+" reais")
+
+
+
