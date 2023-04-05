f92ace11e669b668518945de31827f526372208f
Jamile <milli.santana2017@gmail.com>
2023-04-05 15:59:00 -0300

    refactor: Refatora código para melhor desempenho

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -4,71 +4,63 @@ let meuNome = "Jamile Santana da Silva";
 
 let minhaIdade = 34;
 
-console.log("Meu nome é", meuNome, "e tenho", minhaIdade, "anos.");
+let meusDados = `Meu nome é ${meuNome} e tenho ${minhaIdade} anos.`
+console.log(meusDados);
 
 
 // Exercicio 2
 
 // saldo negativo
+
 let saldo = -35;
-
 let gastos = 786;
-
-let saldoFinal = saldo - gastos;
-
-console.log("Meu saldo final é de", saldoFinal, "reais");
+let resultado = saldo - gastos;
+let saldoFinal = `Meu saldo final é de ${resultado} reais`
+console.log(saldoFinal);
 
 // Quem dera
+
 // let saldo = 1000000;
-
 // let gastos = 50000;
+// let resultado = saldo - gastos;
+// let saldoFinal = `Meu saldo final é de ${resultado} reais`
+// console.log(saldoFinal);
 
-// let saldoFinal = saldo - gastos;
-
-// console.log("Meu saldo final é de", saldoFinal, "reais");
 
 // let saldo = 340;
-
 // let gastos = 340;
-
-// let saldoFinal = saldo - gastos;
-
-// console.log("Meu saldo final é de", saldoFinal, "reais");
+// let resultado = saldo - gastos;
+// let saldoFinal = `Meu saldo final é de ${resultado} reais`
+// console.log(saldoFinal);
 
 
 // Estes cálculos me retornam NaN, pois tenho varíaveis undefined
-// let saldo;
 
+// let saldo;
 // let gastos = 786;
-
-// let saldoFinal = saldo - gastos;
-
-// console.log("Meu saldo final é de", saldoFinal, "reais");
+// let resultado = saldo - gastos;
+// let saldoFinal = `Meu saldo final é de ${resultado} reais`
+// console.log(saldoFinal);
 
 // let saldo;
-
 // let gastos;
-
-// let saldoFinal = saldo - gastos;
-
-// console.log("Meu saldo final é de", saldoFinal, "reais");
+// let resultado = saldo - gastos;
+// let saldoFinal = `Meu saldo final é de ${resultado} reais`
+// console.log(saldoFinal);
 
 
 // Esse cálculo me retorna negativo, poi estou subtraindo meus gastos de 0;
+
 // let saldo = null;
-
 // let gastos = 786;
-
-// let saldoFinal = saldo - gastos;
-
-// console.log("Meu saldo final é de", saldoFinal, "reais");
+// let resultado = saldo - gastos;
+// let saldoFinal = `Meu saldo final é de ${resultado} reais`
+// console.log(saldoFinal);
 
 
 // Este me retorna 0 pois tenho duas variáveis null(0)
 // let saldo = null;
-
 // let gastos = null;
-
-// let saldoFinal = saldo - gastos;
-
-// console.log("Meu saldo final é de", saldoFinal, "reais");
+// let resultado = saldo - gastos;
+// let saldoFinal = `Meu saldo final é de ${resultado} reais`
+// console.log(saldoFinal);
