49254cd92791369948218846efd924d3fc6a17cd
zorydev <129124234+zorydev@users.noreply.github.com>
2023-04-05 22:01:20 -0300

    ExercicioParaCasaEChecklist

--- exercicios/para-casa/exercicios.js
+++ exercicios/para-casa/exercicios.js
@@ -1,5 +1,12 @@
 // Exercicio 1
+let nome = "Tatyane Estrela dos Santos"
+let idade = 38
 
-
+console.log("Meu nome é "+nome+" e tenho "+idade+" anos.")
 
 // Exercicio 2
+let saldo = 2100
+let gastos = 2000
+let saldoFinal = 100
+
+console.log("Meu saldo final é de "+saldoFinal+" reais.")
