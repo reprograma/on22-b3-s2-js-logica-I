78191e6c366c824c64df6bfcf56b73f64e73f49c
Luana <127024993+Luatito@users.noreply.github.com>
2023-04-06 21:25:59 -0300

    fazer commit

---
+++ exercicios/para-casa/exercicio2.js
@@ -0,0 +1,9 @@
+// Exercicio 2
+
+let saldo = 100
+let gastos = 3
+let saldoFinal = saldo - gastos 
+
+let resultado = "Meu saldo final é de " + saldoFinal + " reais"
+
+console.log(resultado)
---
+++ exercicios/para-casa/exercicios.js
@@ -0,0 +1,9 @@
+// Exercicios 1
+
+let nome = "Luana Lima Tito"
+let idade = 24
+
+let mensagem = "Meu nome é " + nome + " e tenho " + idade + " anos"
+
+console.log(mensagem)
+
